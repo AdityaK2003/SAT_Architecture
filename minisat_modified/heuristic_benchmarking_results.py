11195f5a1e82f6f17b48359977d0af324c2b8339
AdityaK2003 <adityak2003@gmail.com>
2025-02-10 16:02:44 -0800

    added more stats to script

--- minisat_modified/heuristic_benchmarking_results.py
+++ minisat_modified/heuristic_benchmarking_results.py
@@ -43,7 +43,7 @@ def parse_heuristic_data(heuristic):
     filenames = sorted(os.listdir(dir_path))
     
     # Create dataframe
-    df = pd.DataFrame(columns = ['cpu_time', 'restarts'])
+    df = pd.DataFrame(columns = ['cpu_time', 'restarts', 'gprof_filename'])
 
     # Loop through files
     i = 0
@@ -82,13 +82,25 @@ def parse_heuristic_data(heuristic):
         restarts = extract_restarts(cnf_out_contents)
 
         # Add info to the dataframe
-        df.loc[cnf_out_prefix] = {'cpu_time': cpu_time, 'restarts': restarts}
+        df.loc[cnf_out_prefix] = {'cpu_time': cpu_time, 'restarts': restarts, 'gprof_filename': gprof_out_file}
 
         # Increment i
         i += 2
 
     # Return the dataframe
     return df
+
+# Combine two dataframes keeping only inputs where both heuristics had outputs
+# Prepends heuristic names to the columns so both heuristics' stats are kept
+def combine_same_inputs(df1, heur1, df2, heur2):
+    # Rename columns by prepending heuristic names
+    df1_renamed = df1.add_prefix(f"{heur1}_")
+    df2_renamed = df2.add_prefix(f"{heur2}_")
+
+    # Perform an inner join on the indices to keep only common rows
+    combined_df = df1_renamed.join(df2_renamed, how='inner')
+
+    return combined_df
 
 
 def main():
@@ -99,14 +111,29 @@ def main():
         print(f'\nParsing heuristic: {heuristic}')
         current_df = parse_heuristic_data(heuristic)
         
-        print('Dataframe Head:')
-        print(current_df.head())
+        # print('Dataframe Head:')
+        # print(current_df.head())
         print(f'Number of files parsed: {len(current_df)}')
 
         # Save the dataframe
         parsed_data[heuristic] = current_df
 
         print()
+
+    # Create combined dataframe with activity and chb stats
+    heur1 = 'activity'
+    heur2 = 'chb'
+    combined_df = combine_same_inputs(parsed_data[heur1], heur1, parsed_data[heur2], heur2)
+    print(f'Combined df for {heur1} and {heur2} has len: {len(combined_df)}\n')
+
+    
+    # Find how many files activity has a better time than chb, and vice versa
+    activity_faster_count = (combined_df['activity_cpu_time'] < combined_df['chb_cpu_time']).sum()
+    print(f'Occurrences of activity_cpu_time < chb_cpu_time: {activity_faster_count}')
+    print(f'Occurrences of chb_cpu_time < activity_cpu_time: {len(combined_df) - activity_faster_count}')
+    print()
+
+    print(combined_df.columns)
 
 
 if __name__ == '__main__':
